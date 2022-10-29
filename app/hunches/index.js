50910f84014c61d10967c9ac2adc0912be18ab20
MariaMuniz <cidamuniz2011@yahoo.com.br>
2022-10-29 17:50:04 -0300

    projeto criado

---
+++ app/hunches/index.js
@@ -0,0 +1,68 @@
+import jwt from 'jsonwebtoken';
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+export const create = async (ctx) => {
+  const { gameId, homeTeamScore, awayTeamScore } = ctx.request.body;
+  const authorization = ctx.headers.authorization;
+
+  if (!authorization) {
+    ctx.status = 403;
+    return;
+  }
+
+  const [, token] = ctx.headers.authorization.split(" ");
+  let userId;
+
+  // Verifica campos obrigatórios
+  if (homeTeamScore === null && awayTeamScore === null) {
+    ctx.status = 400;
+    return;
+  }
+
+  // Verifica se o token JWT é válido
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    userId = decoded.sub;
+
+  } catch (error) {
+    ctx.status = 403;
+    return;
+  }
+
+  const data = {
+    userId,
+    gameId,
+    homeTeamScore: homeTeamScore,
+    awayTeamScore: awayTeamScore
+  }
+
+  try {
+    let hunch;
+    const exists = await prisma.hunch.findFirst({
+      where: { userId, gameId }
+    });
+
+    if (exists) {
+      hunch = await prisma.hunch.updateMany({
+        where: { userId, gameId },
+        data: {
+          homeTeamScore,
+          awayTeamScore
+        }
+      })
+    }
+    else {
+      hunch = await prisma.hunch.create({ data });
+    }
+
+    ctx.body = hunch;
+    ctx.status = 200;
+
+  } catch (error) {
+    console.warn(error)
+    ctx.body = error;
+    ctx.status = 500;
+  }
+}
